f77e3280581e30fb6f0f9c769111bf6a130628d0
zsy <zsyyltf@126.com>
2020-12-01 17:44:12 +0800

    test-0.06

--- datawarehouse-realtime/src/main/scala/com/lavideo/utils/MyKafkaUtil1.scala
+++ datawarehouse-realtime/src/main/scala/com/lavideo/utils/MyKafkaUtil1.scala
@@ -9,8 +9,8 @@ import org.apache.flink.streaming.api.scala._
 
 object MyKafkaUtil1 {
     private val prop = new Properties()
-//    prop.setProperty("bootstrap.servers","10.10.3.91:9092,10.10.3.92:9092,10.10.3.93:9092")
-    prop.setProperty("bootstrap.servers","localhost:9092")
+    prop.setProperty("bootstrap.servers","10.10.3.91:9092,10.10.3.92:9092,10.10.3.93:9092")
+//    prop.setProperty("bootstrap.servers","localhost:9092")
 
     prop.setProperty("group.id","order-num")
     prop.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
